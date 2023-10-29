c59499dd5192ee3110adf3fef0696ed89c251b24
sreerajreddy <139043578+sreerajreddy@users.noreply.github.com>
2023-10-29 15:04:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sreerajreddy/simple-pythonn-app
+docker pull sreerajreddy/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sreerajreddy/simple-pythonn-app
+docker run -d -p 5000:5000 sreerajreddy/simple-python-app
